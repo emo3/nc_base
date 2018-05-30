9e82860814ce25e34cee37056db60f70c2359956
Ed Overton <e.overton@gmail.com>
2018-05-29 21:47:44 -0400

    more generic

--- recipes/make_nc_base.rb
+++ recipes/make_nc_base.rb
@@ -1,5 +1,9 @@
 # fix anything with RHEL that is needed for netcool
 include_recipe '::fix_nc_base'
+# create netcool accounts
+include_recipe '::create_nc_acct'
+# install installation Manager
+include_recipe '::install_im'
 # install base netcool
 include_recipe '::install_nco81'
 # Update Netcool to latest fix pack
