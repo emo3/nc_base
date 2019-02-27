2c24b869c9aa0e564d8c53f62e90ed9f1fa30394
Ed Overton <e.overton@gmail.com>
2019-02-26 20:52:08 -0500

    fix copyright, etc

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'Apache 2.0'
 description 'Installs/Configures nc_base'
 long_description 'Installs/Configures nc_base'
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 13.0'
 supports 'redhat'
 
 # The `issues_url` points to the location where issues for this cookbook are
@@ -20,6 +20,5 @@ supports 'redhat'
 #
 # source_url 'https://github.com/<insert_org_here>/nc_base'
 depends 'server_utils', '~> 0.1.0'
-# depends 'delivery-truck'
 depends 'limits'
 depends 'selinux'
