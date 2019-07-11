09b1d7edaa6c7fd63920447381073fe19ffccf6d
Ed Overton <e.overton@gmail.com>
2019-07-11 10:20:21 -0400

    fix changes to limits

--- recipes/fix_nc_base.rb
+++ recipes/fix_nc_base.rb
@@ -19,66 +19,63 @@ end
 # Install RPM's
 package node['nc_base']['rhel']
 
-# set ulimits needed for ibm_apm
+# set ulimits needed for netcool
+## Create file to hold limits
+limits_file '/etc/security/limits.conf' do
+  action :create
+end
+
 ## max number of processes
-set_limit '*' do
+limit 'limit-processes-hard' do
   type 'hard'
   item 'nproc'
   value 'unlimited'
-  use_system true
 end
 
-set_limit '*' do
+limit 'limit-processes-soft' do
   type 'soft'
   item 'nproc'
   value 'unlimited'
-  use_system true
 end
 
 ## max number of open file descriptors
-set_limit '*' do
+limit 'limit-files-hard' do
   type 'hard'
   item 'nofile'
   value 33000
-  use_system true
 end
 
-set_limit '*' do
+limit 'limit-files-soft' do
   type 'soft'
   item 'nofile'
   value 33000
-  use_system true
 end
 
 ## limits the core file size (KB)
-set_limit '*' do
+limit 'limit-core-hard' do
   type 'hard'
   item 'core'
   value 390001
-  use_system true
 end
 
-set_limit '*' do
+limit 'limit-core-soft' do
   type 'soft'
   item 'core'
   value 390001
-  use_system true
 end
 
 ## maximum filesize (KB)
-set_limit '*' do
+limit 'limit-filesize-soft' do
   type 'soft'
   item 'fsize'
   value 'unlimited'
-  use_system true
 end
 
 ## max stack file size
-set_limit '*' do
+limit 'limit-stack-soft' do
   type 'soft'
   item 'stack'
   value 'unlimited'
-  use_system true
 end
 
 # make sure ntp is up and running
