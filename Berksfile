cf1356fd8a885da56574bc189d71669a9cf67160
Ed Overton <e.overton@gmail.com>
2020-05-15 22:58:53 -0400

    remove Berksfile kitchen.yml bump metadata this is ONLY a routine, do use to build any box

--- Berksfile
+++
@@ -1,5 +0,0 @@
-source 'https://supermarket.chef.io'
-metadata
-cookbook 'delivery-truck', github: 'chef-cookbooks/delivery-truck'
-cookbook 'delivery-sugar', github: 'chef-cookbooks/delivery-sugar'
-cookbook 'server_utils', path: '../server_utils'
--- metadata.rb
+++ metadata.rb
@@ -1,24 +1,16 @@
 name 'nc_base'
 maintainer 'Ed Overton'
-maintainer_email 'infuse.1301@gmail.com'
+maintainer_email 'bogus@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures nc_base'
 long_description 'Installs/Configures nc_base'
-version '0.2.0'
+version '0.3.0'
 chef_version '>= 13.0'
 supports 'redhat'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/nc_base/issues' if respond_to?(:issues_url)
+issues_url 'https://github.com/emo3/nc_base/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/emo3/nc_base'
 
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/nc_base'
 depends 'server_utils'
 depends 'limits'
 depends 'selinux'
