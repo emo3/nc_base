d27894c946ad554d1924d707eb4bb4f63f11b052
Ed Overton <e.overton@gmail.com>
2020-09-08 19:43:59 -0400

    update ObjectServer to v23

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
 default['nc_base'].tap do |ncb|
   ncb['base_ver']    = '8.1.0'
   ncb['point_ver']   = "#{node['nc_base']['base_ver']}.1"
-  ncb['fp_ver']      = '22'
+  ncb['fp_ver']      = '23'
   ncb['fp_pkg']      = "#{node['nc_base']['base_ver']}-TIV-NCOMNIbus-Linux-FP00#{node['nc_base']['fp_ver']}.zip"
-  ncb['fp_rel']      = '5.50.90.20200205_2042'
+  ncb['fp_rel']      = '5.50.92.20200703_1706'
   ncb['package']     = "OMNIbus-v#{node['nc_base']['point_ver']}-Core.linux64.zip"
   ncb['im_ver']      = '1.9.1003.20200730_2125'
   ncb['im_pkg']      = "agent.installer.linux.gtk.x86_64_#{node['nc_base']['im_ver']}.zip"
